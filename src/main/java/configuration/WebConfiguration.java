f58ab679851d25fccce90971a4653731ee2f4014
Simon Pirko <sipi7gm@gmail.com>
2020-07-15 11:11:06 +0300

    Add web configuration class

---
+++ src/main/java/configuration/WebConfiguration.java
@@ -0,0 +1,12 @@
+package configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfiguration extends WebMvcConfigurationSupport {
+
+}
