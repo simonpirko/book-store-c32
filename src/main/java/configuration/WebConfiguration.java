a25d4dcea91cde2f2d0668b9419338aea5308e42
Simon Pirko <sipi7gm@gmail.com>
2020-07-16 20:49:31 +0300

    Add ViewResolver

--- src/main/java/configuration/WebConfiguration.java
+++ src/main/java/configuration/WebConfiguration.java
@@ -1,12 +1,21 @@
 package configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 public class WebConfiguration extends WebMvcConfigurationSupport {
 
+    @Bean
+    public InternalResourceViewResolver viewResolver(){
+        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+        internalResourceViewResolver.setPrefix("/pages/");
+        internalResourceViewResolver.setSuffix(".jsp");
+        return internalResourceViewResolver;
+    }
 }
