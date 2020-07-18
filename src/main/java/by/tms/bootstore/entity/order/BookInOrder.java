dc28df981b4362b2d604f051c70424b9e6da30f5
IseTheSal <illya.ageevich@gmail.com>
2020-07-18 18:12:10 +0300

    Added PaymentMethod; Filled enum.

--- src/main/java/by/tms/bootstore/entity/order/BookInOrder.java
+++ src/main/java/by/tms/bootstore/entity/order/BookInOrder.java
@@ -2,8 +2,11 @@ package by.tms.bootstore.entity.order;
 
 import by.tms.bootstore.entity.books.Book;
 
+
 public class BookInOrder {
 
     private long id;
     private Book book;
+    private double countOfBook;
+
 }
