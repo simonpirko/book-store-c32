6fe5b1e37674f75eab64191c35a58c43a25ea74d
LukshaMikalai <lucshahomm@gmail.com>
2020-07-21 11:20:40 +0300

    commit

--- src/main/java/by/tms/bootstore/controller/BookController.java
+++ src/main/java/by/tms/bootstore/controller/BookController.java
@@ -1,4 +1,16 @@
 package by.tms.bootstore.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/book")
 public class BookController {
+
+
+    @PostMapping(path = "/addc")
+    public String addBook(){
+
+    }
 }
